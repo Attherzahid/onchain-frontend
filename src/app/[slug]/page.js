74578012233555cd5ee0413c1362ba42bf4e8442
digitalogiesather <digitalogies.ather@gmail.com>
2024-08-19 19:29:34 +0500

    add dynamic page through api.

---
+++ src/app/[slug]/page.js
@@ -0,0 +1,43 @@
+import { notFound } from 'next/navigation';
+import DefalutSec from '../sections/DefaultSec';
+import { Footer, Header } from '../components';
+
+export default async function Page({ params }) {
+    const { slug } = params;
+
+    // Fetch the data for the page from WordPress
+    const res = await fetch(`http://local.onchain-backend.com/wp-json/wp/v2/pages?slug=${slug}`);
+    const data = await res.json();
+    const page = data.length ? data[0] : null;
+
+    // If no page is found, return a 404
+    if (!page) {
+        notFound();
+    }
+
+    const cleanedContent = page.content.rendered.replace(/class="[^"]*"/g, '');
+
+    return (
+
+        <>
+            <Header />
+            <main>
+                <DefalutSec>
+                    <h1 className='h2 gradient-text'>{page.title.rendered}</h1>
+                    <div dangerouslySetInnerHTML={{ __html: cleanedContent }} />
+                </DefalutSec>
+            </main>
+
+            <Footer />
+        </>
+    );
+}
+
+export async function generateStaticParams() {
+    const res = await fetch('http://local.onchain-backend.com/wp-json/wp/v2/pages');
+    const pages = await res.json();
+
+    return pages.map((page) => ({
+        slug: page.slug,
+    }));
+}
